f4f59b9c43c5870b3030fe776ea9a83f0991521f
ShrirajChohan32 <shriraj.chohan@myob.com>
2025-08-27 13:00:29 +1000

    Checking if Buildkite pipeline works

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import aws_cdk as cdk
 from blog.blog_stack import BlogStack
 
-env_dev = cdk.Environment(account="1234567890",region="ap-southeast-2")
+env_dev = cdk.Environment(account="303377997640",region="ap-southeast-2")
 
 app = cdk.App()
 
